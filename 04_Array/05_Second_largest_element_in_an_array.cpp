4949e3652a87758eab9af6de7635eccef4dc7126
Vivek Sarade <viveksarade4949@gmail.com>
2022-10-28 00:13:04 +0530

    added one traversal solution

--- 04_Array/05_Second_largest_element_in_an_array.cpp
+++ 04_Array/05_Second_largest_element_in_an_array.cpp
@@ -32,10 +32,31 @@ int secondLargest(int arr[], int n)
     return res;
 }
 
+// One traversal solution
+int secondLargest2(int arr[], int n)
+{
+    int res=-1, largest=0;
+    for(int i=1; i<n; i++)
+    {
+        if(arr[i]>arr[largest])
+        {
+            res=largest;
+            largest=i;
+        }
+        else if(arr[i]!=arr[largest])
+        {
+            if(res==-1 || arr[i]>arr[res])
+                res=i;
+        }
+    }
+    return res;
+}
+
 int main()
 {
-    int arr[5]={5, 45, 52, 42, 10};
+    int arr[5]={100, 45, 52, 42, 10};
     int n=5;
-    cout<<secondLargest(arr, n);
+    cout<<secondLargest(arr, n)<<endl;
+    cout<<secondLargest2(arr, n)<<endl;
     return 0;
 }
