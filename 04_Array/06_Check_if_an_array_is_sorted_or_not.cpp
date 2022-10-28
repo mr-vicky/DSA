ad70a0d006724c441c1bc0ede8c85a739a78ba14
Vivek Sarade <viveksarade4949@gmail.com>
2022-10-28 23:03:12 +0530

    added program file which checks array is sorted or not

---
+++ 04_Array/06_Check_if_an_array_is_sorted_or_not.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+// Method 1
+bool isSorted(int arr[], int n)
+{
+    int temp=0;
+    for(int i=1; i<n; i++)
+        if(arr[i]>arr[temp])
+            temp=i;
+        else if(arr[i]<arr[temp])
+            return false;
+
+    return true;
+}
+
+// Method 2
+bool isSorted2(int arr[], int n) 
+{
+    for(int i=0; i<n; i++)
+        if(arr[i]<arr[i-1])
+            return false;
+
+    return true;
+}
+
+
+int main()
+{
+    int arr[5]={1, 5, 13, 8, 7};
+    int n=5;
+    cout<<boolalpha<<isSorted(arr, n)<<endl;
+    cout<<boolalpha<<isSorted2(arr, n)<<endl;
+
+    return 0;
+}
