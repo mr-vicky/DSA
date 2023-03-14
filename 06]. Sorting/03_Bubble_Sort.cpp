a06f9c06cbe01a2a08afe968310be48fc2c4a7f1
Vivek Sarade <viveksarade4949@gmail.com>
2023-03-14 16:12:53 +0530

    Sorting

--- 06]. Sorting/03_Bubble_Sort.cpp
+++ 06]. Sorting/03_Bubble_Sort.cpp
@@ -8,8 +8,8 @@ void BubbleSort(int arr[], int n)
 {
     for(int i=0; i<n-1; i++)
         for(int j=0; j<n-i-1; j++)
-            if(arr[i]>arr[j])
-                swap(arr[i], arr[j+1]);
+            if(arr[j]>arr[j+1])
+                swap(arr[j], arr[j+1]);
 }
 
 int main()
