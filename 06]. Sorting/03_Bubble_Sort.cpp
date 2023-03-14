b50fb429f3c897d0c2c92fc48debeb1e0faa5358
Vivek Sarade <viveksarade4949@gmail.com>
2023-03-14 16:02:52 +0530

    Sorting

---
+++ 06]. Sorting/03_Bubble_Sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+// TC: O(n^2)
+// SC: O(1)
+void BubbleSort(int arr[], int n)
+{
+    for(int i=0; i<n-1; i++)
+        for(int j=0; j<n-i-1; j++)
+            if(arr[i]>arr[j])
+                swap(arr[i], arr[j+1]);
+}
+
+int main()
+{
+    int n = 4;
+    int arr[n]={2, 10, 8, 7};
+    cout<<"Array before Sorting : "<<endl;
+    for(auto x: arr)
+        cout<<x<<" ";
+
+    BubbleSort(arr, n);
+
+    cout<<"\nArray after Sorting by using Bubble sort: "<<endl;
+    for(auto x: arr)
+        cout<<x<<" ";
+    return 0;
+}
