d1f5f5d24f72ed0384af804915e534a6804ff7b9
Vivek Sarade <viveksarade4949@gmail.com>
2023-03-18 14:46:36 +0530

    Sorting

--- 06]. Sorting/03_Bubble_Sort.cpp
+++ 06]. Sorting/03_Bubble_Sort.cpp
@@ -2,14 +2,38 @@
 #include<algorithm>
 using namespace std;
 
+// Naive Method:
 // TC: O(n^2)
 // SC: O(1)
-void BubbleSort(int arr[], int n)
+void BubbleSort1(int arr[], int n)
 {
     for(int i=0; i<n-1; i++)
         for(int j=0; j<n-i-1; j++)
             if(arr[j]>arr[j+1])
                 swap(arr[j], arr[j+1]);
+}
+
+
+// Optimized Bubble Sort : 
+// If the array is sorted in middle 
+void BubbleSort2(int arr[], int n)
+{
+    bool isSwapped;
+    for(int i=0; i<n-1; i++)
+    {
+        isSwapped = false;
+        for(int j=0; j<n-i-1; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j], arr[j+1]);
+                isSwapped = true;
+            }
+        }
+
+        if(isSwapped == false)
+            break;
+    }
 }
 
 int main()
@@ -20,7 +44,11 @@ int main()
     for(auto x: arr)
         cout<<x<<" ";
 
-    BubbleSort(arr, n);
+    // Naive Method :
+    BubbleSort1(arr, n);
+
+    // Optimized Bubble Sort : 
+    BubbleSort2(arr, n);
 
     cout<<"\nArray after Sorting by using Bubble sort: "<<endl;
     for(auto x: arr)
