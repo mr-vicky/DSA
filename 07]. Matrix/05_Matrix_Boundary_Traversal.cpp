8ede4fd2ed86ce142bb722cff452d3119da3b5d1
Vivek Sarade <viveksarade4949@gmail.com>
2023-04-15 11:30:44 +0530

    Matrix

--- 07]. Matrix/05_Matrix_Boundary_Traversal.cpp
+++ 07]. Matrix/05_Matrix_Boundary_Traversal.cpp
@@ -5,31 +5,33 @@ void getBoundaryTraversal(int mat[4][4])
 {
     int R = 4;
     int C = 4;
-    for(int i=0; i<R; i++)
+    
+    if(R==1)
+        for(int i=0; i<C; i++)
+            cout<<mat[0][i]<<" ";
+    else if(C==1)
+        for(int i=0; i<R; i++)
+            cout<<mat[i][0]<<" ";
+    else
     {
-        if(i==0)
-        {
-            for(int j=0; j<R; j++)
-            {
-                cout<<mat[i][j]<<" ";
-            }
-        }
-        else if(i==C-2)
-        {
-            for(int j=1; j<R; j++)
-            {
-                cout<<mat[i][j]
-            }
-        }
+        for(int i=0; i<C; i++)
+            cout<<mat[0][i]<<" ";
+        for(int i=1; i<R; i++)
+            cout<<mat[i][C-1]<<" ";
+        for(int i=C-2; i>=0; i--)
+            cout<<mat[R-1][i]<<" ";
+        for(int i=R-2; i>=1; i--)
+            cout<<mat[i][0]<<" ";
     }
 }
 
 
 int main()
 {
-    int mat[4][4]={{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
+    int mat1[4][4]={{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
 
-    getBoundaryTraversal(mat);
+    getBoundaryTraversal(mat1);
+    cout<<endl;
 
     return 0;
 }
--- 07]. Matrix/README.md
+++ 07]. Matrix/README.md
@@ -5,4 +5,5 @@
 - 01]. [Declaration of Matrix Part - 1](https://github.com/mr-vicky/DSA/blob/main/07%5D.%20Matrix/01_Declaration_of_Matrix_part_1.cpp.cpp)
 - 02]. [Declaration of Matrix Part - 2](https://github.com/mr-vicky/DSA/blob/main/07%5D.%20Matrix/02_Declaration_of_Matrix_part_2.cpp)
 - 03]. [Passing 2D arrays as Arguments](https://github.com/mr-vicky/DSA/blob/main/07%5D.%20Matrix/03_Passing_2D_arrays_as_arguments.cpp)
-- 04]. [Problem : Matrix in Snake Pattern](https://github.com/mr-vicky/DSA/blob/main/07%5D.%20Matrix/04_Matrix_in_Snake_Pattern.cpp)
+- 04]. [Problem - Matrix in Snake Pattern](https://github.com/mr-vicky/DSA/blob/main/07%5D.%20Matrix/04_Matrix_in_Snake_Pattern.cpp)
+- 05]. [Problem - Matrix Boundary Traversal]()
